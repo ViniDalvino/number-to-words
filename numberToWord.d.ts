c97cb3d06ebb17285346aa6839c02e648d75c284
Vini Dalvino <37189791+ViniDalvino@users.noreply.github.com>
2020-12-05 17:05:50 -0500

    Create numberToWord.d.ts

---
+++ numberToWord.d.ts
@@ -0,0 +1,3 @@
+export function toOrdinal(number: number | string): string
+export function toWords(number: number | string, asOrdinal?: boolean): string;
+export function toWordsOrdinal(number: number | string): string
